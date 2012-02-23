e7e2e8736dadd43394043d83243fc657d64ecbdc
Nik Cubrilovic <nikcub@gmail.com>
2012-02-23 15:48:22 +1100

    initial win32 support

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ if getattr(sys, 'version_info', (0, 0, 0)) < (2, 5, 0, 'final'):
     raise SystemExit("floyd requires Python 2.5 or later.")
 
 from setuptools import setup
-    
+
+scripts = ['bin/floyd']
+
+if os.name == 'nt':
+  scripts.append('bin/floyd.bat')
+
 setup(
   name="floyd",
   description='Command line static website generator for cloud hosts',
@@ -15,5 +20,5 @@ setup(
   author_email='nikcub@gmail.com',
   license='BSD 2-clause',
   packages=['floyd'],
-  scripts=['bin/floyd'],
+  scripts=scripts,
 )
