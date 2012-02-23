52a2f04c7797f751a1de06182ec14773b9df6cc5
Nik Cubrilovic <nikcub@gmail.com>
2012-02-23 15:39:26 +1100

    additional setup variables

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
-from setuptools import setup
+import sys
+import platform
 
-setup(name="floyd",
+if getattr(sys, 'version_info', (0, 0, 0)) < (2, 5, 0, 'final'):
+    raise SystemExit("floyd requires Python 2.5 or later.")
+
+from setuptools import setup
+    
+setup(
+  name="floyd",
   description='Command line static website generator for cloud hosts',
   version='0.1',
+  author='Nik Cubrilovic',
+  author_email='nikcub@gmail.com',
+  license='BSD 2-clause',
   packages=['floyd'],
   scripts=['bin/floyd'],
 )
