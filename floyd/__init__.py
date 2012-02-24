3e0ca8443b95dca29ba884c93da506e505dd22b2
Nik Cubrilovic <nikcub@gmail.com>
2012-02-24 19:14:34 +1100

    moved package variables and applied to setup

--- floyd/__init__.py
+++ floyd/__init__.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+"""A command line static website generator for popular cloud hosting providers
+
+:copyright: Copyright (C) 2012 Nik Cubrilovic and others, see AUTHORS
+:license: new BSD, see LICENSE for more details.
+
+"""
+__clsname__ = 'floyd'
+__version__ = '0.0.1'
+__author__ = 'Nik Cubrilovic <nikcub@gmail.com>'
+__email__ = 'nikcub@gmail.com'
+__url__ = 'http://github.com/nikcub/Floyd'
+__license__ = 'BSD'
+
 if __name__ == '__main__':
   sys.exit(floyd.main.run())
 
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import platform
+import floyd
 
 if getattr(sys, 'version_info', (0, 0, 0)) < (2, 5, 0, 'final'):
     raise SystemExit("floyd requires Python 2.5 or later.")
@@ -14,12 +15,19 @@ if os.name == 'nt':
   scripts.append('bin/floyd.bat')
 
 setup(
-  name="floyd",
-  description='Command line static website generator for cloud hosts',
-  version='0.1',
-  author='Nik Cubrilovic',
-  author_email='nikcub@gmail.com',
-  license='BSD 2-clause',
+  name=floyd.__clsname__,
+  description=floyd.__doc__.split('\n\n')[0],
+  long_description=open('README.md').read(),
+  keywords='website, appengine, s3, cms, blog',
+  url=floyd.__url__,
+  platforms=['linux', 'osx', 'win32'],
+  version=floyd.__version__,
+  author=floyd.__author__,
+  author_email=floyd.__email__,
+  license=floyd.__license__,
+  install_requires=[
+    'jinja2'
+  ],
   packages=['floyd'],
   scripts=scripts,
 )
