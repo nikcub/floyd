e0f15749f4c91e155f943f6e93fbbb5e074a94f3
Nik Cubrilovic <nikcub@gmail.com>
2012-03-11 23:56:25 +1100

    implementing new controllers

--- floyd/controllers.py
+++ floyd/controllers.py
@@ -6,21 +6,41 @@ import jinja2
 from templating import jinja
 from floyd.util.timesince import timesince
 
-      # jinja.setup(self.config.paths.template_sets)
-      # 
-      # # setup variables
-      # passed_vars = self.get_template_vars(passed_vars)
-      # passed_vars = self.get_plugin_vars(passed_vars)
-      # 
-      # # Get template_set and template_theme
-      # template_set = self.get_template_set()
-      # template_theme = self.get_template_theme(template_set)
-      # 
-      # # logging.info('Rendering with: template_name: %s template_theme: %s template_set: %s and variables:' % (template_name, template_theme, template_set))
-      # # logging.info(passed_vars)
-      # 
-      # content = jinja.render(template_name, passed_vars, template_theme=template_theme, template_set=template_set)
+class BaseController(object):
+  pass
 
+def index():
+  return floyd.render('index')
+
+
+def TemplateVars(vars):
+  additional = {
+    'admin': False,
+    'user': None,
+    'logout': '/',
+    'login': '/',
+    'static_host': 'sketch-proto.appspot.com',
+    'css_file': 'nikcub.030.min.css',
+    'css_ver': '26',
+    'src': 'database',
+  }
+  return dict(zip(additional.keys() + vars.keys(), additional.values() + vars.values()))
+
+def RenderPosts(posts, outputdir):  
+  for post in posts:
+    print 'rendering -- %s' % post.stub
+    template_vars = {}
+    template_vars['posts'] = posts[:5]
+    template_vars['post'] = post
+    template_vars = TemplateVars(template_vars)
+    
+    render = floyd.templating.jinja.render('single', template_vars)
+    post_output_path = os.path.join(outputdir, post.stub)
+    print 'render: %s ' % post_output_path
+    fp = open(post_output_path, 'w')
+    fp.write(render)
+    fp.close()
+    
 def render(template_name, template_path, template_variables, output_file):
   template = os.path.join(template_path, template_name)
   
@@ -30,8 +50,3 @@ def render(template_name, template_path, template_variables, output_file):
   content = jinja.render(template_name, passed_vars, template_theme=template_theme, template_set=template_set)
   return content
   
-  # env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
-  # env.filters['timesince'] = timesince
-  # env.filters['tformat'] = timeformat
-  # jinja_template = env.get_template(template_name)
-  # return jinja_template.render(template_vars)
